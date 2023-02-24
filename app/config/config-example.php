1142742c2d205ecf97b1aef9c66a1d90a415a647
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-02-24 14:19:37 +0100

    correct database name
    correct name off the database

--- app/config/config-example.php
+++ app/config/config-example.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'restaurant');
+define('DB_NAME', 'rocambolesque');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 

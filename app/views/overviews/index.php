9c03cf3b0c7222e7b2b14cbf48007ca17f819829
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-03-10 15:09:19 +0100

    Create index.php
    menu page index

---
+++ app/views/overviews/index.php
@@ -0,0 +1,75 @@
+<link rel="stylesheet" href="/css/menu.css">
+
+<div class="main">
+  <br> <br>
+  <h1>Kinderen</h1>
+  <br> <br>
+  <ul class="cards">
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/croisant.jpg"></div>
+        <div class="card_content">
+          <h2 class="card_title">Croissants 3 stuks</h2>
+          <p class="card_text">3 overheerlijke croissants</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/crepe2.png"></div>
+        <div class="card_content">
+          <h2 class="card_title">Crepe chocolate</h2>
+          <p class="card_text">Crepe met aardbei en chocolade</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/crepe2.png"></div>
+        <div class="card_content">
+          <h2 class="card_title">Crepe chocolate</h2>
+          <p class="card_text">Crepe met aardbei en chocolade</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+    </ul>
+    <br> <br>
+<h1>Volwassenen</h1>
+<br> <br>
+<ul class="cards">
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/croisant.jpg"></div>
+        <div class="card_content">
+          <h2 class="card_title">Croissants 6 stuks</h2>
+          <p class="card_text">6 overheerlijke croissants</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/crepe2.png"></div>
+        <div class="card_content">
+          <h2 class="card_title">Crepe chocolate 1 stuks</h2>
+          <p class="card_text">Crepe met aardbei en chocolade</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="/img/crepe2.png"></div>
+        <div class="card_content">
+          <h2 class="card_title">Crepe chocolate 2 stuks</h2>
+          <p class="card_text">Crepe met aardbei en chocolade</p>
+          <button class="btn card_btn">Reserveren</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+

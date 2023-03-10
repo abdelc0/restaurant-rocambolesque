adeeaa682ec0908bc3152669e4129b7257dea77d
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2023-03-10 15:19:02 +0100

    homepage is af

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -9,7 +9,7 @@
 
     <!-- Styling for the footer and navbar -->
     <link rel="stylesheet" href="/css/nav-foot.css">
-
+    <link rel="stylesheet" href="css/home.css">
     <!-- Hamburger link for small screens -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <title><?= SITENAME; ?></title>

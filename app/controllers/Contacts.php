36df1fcd52285d920eb79d927b58f8029ebffb71
Saad332556 <332556@student.mboutrecht.nl>
2023-03-17 15:24:24 +0100

    small changes, corrected some small faults/errors

--- app/controllers/Contacts.php
+++ app/controllers/Contacts.php
@@ -1,6 +1,7 @@
 <?php
 class Contacts extends Controller
 {
+  private $contactModel;
 
   public function __construct()
   {
@@ -9,13 +10,15 @@ class Contacts extends Controller
 
   public function contact()
   {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $this->view('homepages/contact');
+    } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         try {
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
             $createdcontact = $this->contactModel->createContact($_POST);
 
-            if (isset($createdcontact) && $createdcontact = true) {
+            if ($createdcontact) {
                 $data = ['success' => 'Uw contactformulier is aangekomen, wij nemen binnenkort contact met u op.'];
                 $this->view('homepages/contact', $data);
             } else {
